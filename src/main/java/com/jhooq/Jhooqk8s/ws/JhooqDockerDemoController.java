e46ef1458637285d103d70e955408079a95fff2b
Malleswari9 <95434162+Malleswari9@users.noreply.github.com>
2023-11-22 17:22:49 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,13 +1,13 @@
-package com.jhooq.Jhooqk8s.ws;
+package com.devops.devopsk8s.ws;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JhooqDockerDemoController {
+public class devopsDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq-k8s i Have updated the message";
+        return "Hello - devops-k8s i Have updated the message";
     }
 }
